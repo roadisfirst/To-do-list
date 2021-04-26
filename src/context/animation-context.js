befc4e29a221636ea0e854799965f8a0b55b1af5
roadisfirst <roadisfirst@gmail.com>
2021-04-26 22:03:13 +0300

    don't need it yet

--- src/context/animation-context.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const AnimationContext = React.createContext({
-    withAnimation: false,
-    start: () => {},
-    stop: () => {}
-})
-
-export default AnimationContext;
